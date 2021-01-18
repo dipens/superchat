b510d26fb660305f40d5edac31f8fa2636115a21
dipens <dipenmshah8@gmail.com>
2021-01-18 10:44:35 -0500

    using ENV vars

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth';
-import firebasejson from './firebase.json'
 
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
+require('dotenv').config()
+
+const firebasejson = {
+  "apiKey": process.env.REACT_APP_FIREBASE_API_KEY,
+  "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
+  "projectId": process.env.REACT_APP_PROJECT_ID,
+  "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
+  "messagingSenderId": process.env.REACT_APP_SENDER_ID,
+  "appId": process.env.REACT_APP_APP_ID,
+}
 
 firebase.initializeApp(
   firebasejson
